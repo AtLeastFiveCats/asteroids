074fa296e366b83da2480d80587bcf0f2541f260
Atleastfivecats <levi.dye01@gmail.com>
2025-03-02 22:43:23 -0700

    changed the position of the quit code

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from constants import *
 
 def main():
     pygame.init()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
     print(f"Starting Asteroids! \nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True: 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0, 0, 0))
         pygame.display.flip()
 
